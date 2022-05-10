3299659cef5953ce7684a6f234c464d6ca66bb2b
bella <luobei@kemai.cn>
2022-05-10 11:07:28 +0800

    feat: [ QIANBAO-495 ] 详情页样式调整

--- src/views/MerchantInfo/KmTechPolicy.vue
+++ src/views/MerchantInfo/KmTechPolicy.vue
@@ -7,17 +7,25 @@
 </script>
 
 <template>
-  <div class="merchantInfo-wrap">
-    这是详情
-    <div class="merchantInfo-back">
+  <div class="kmtechPolicy-wrap">
+    <div class="kmtechPolicy-main-wrap">这是详情</div>
+    <div class="kmtechPolicy-back">
       <van-button type="default" @click="handleBack">返回</van-button>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
-  .merchantInfo-back {
+  .kmtechPolicy-main-wrap {
+    height: 100%;
+    // background-color: $bg-light-color-1;
+  }
+  .kmtechPolicy-back {
     padding: 24px 32px;
+    box-sizing: border-box;
     background-color: #fff;
+    position: fixed;
+    bottom: 0;
+    width: 100vw;
     .van-button {
       width: 100%;
       background-color: $bg-light-color-1;
