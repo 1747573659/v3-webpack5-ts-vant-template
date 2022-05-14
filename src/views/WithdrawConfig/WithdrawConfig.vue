3ad90fb9aafebb50cb4bda982b643a7eea5bed5c
bella <luobei@kemai.cn>
2022-05-14 09:40:26 +0800

    feat: [QIANBAO-505]提现配置修改功能调整

--- src/views/WithdrawConfig/WithdrawConfig.vue
+++ src/views/WithdrawConfig/WithdrawConfig.vue
@@ -1,34 +1,43 @@
 <template>
   <div class="withdrawConfig-wrap">
     <div class="withdrawConfig-header-wrap">
-      <div class="withdrawConfig-header">
-        <div class="withdrawConfig-header-left">
-          <div class="withdrawConfig-header-title">定时提现</div>
-          <div class="withdrawConfig-tips" v-if="state.planType === 0">
-            设置每天固定提现时间，提现当前账户全部余额
+      <div class="withdrawConfig-header-inner">
+        <div class="withdrawConfig-header">
+          <div>
+            <div class="withdrawConfig-header-title">定时提现</div>
+            <div class="withdrawConfig-tips" v-if="state.planType === 0">
+              设置每天固定提现时间，提现当前账户全部余额
+            </div>
           </div>
-          <div class="withdrawConfig-tips-timing" v-if="state.planType === 1">
-            每天 {{ state.planTime }}
+          <van-switch
+            :modelValue="state.planType"
+            size="24px"
+            :active-value="1"
+            :inactive-value="0"
+            @change="handleSwitchChange" />
+        </div>
+        <div class="withdrawConfig-edit" v-if="state.planType === 1">
+          <div class="withdrawConfig-tips-timing">每天 {{ state.planTime }}</div>
+          <div class="withdrawConfig-icon-wrap" @click="() => (timePickerVisible = true)">
+            <img class="withdrawConfig-icon-info" src="../../assets/img/edit.png" alt="" />
+            修改
           </div>
         </div>
-        <van-switch
-          :modelValue="state.planType"
-          size="24px"
-          :active-value="1"
-          :inactive-value="0"
-          @change="handleSwitchChange" />
       </div>
     </div>
     <div class="withdrawConfig-tips withdrawConfig-footer">
-      <div><van-icon name="warning-o" />温馨提示：</div>
-      <div>1、设置定时提现后，仍可点击提现钮，手动提现</div>
-      <div>2、定时提现时间设置完成后，每天届时会把当前账户可提现余额，全部提现</div>
+      <div>
+        <img class="withdrawConfig-icon-info" src="../../assets/img/info-o.png" alt="" />
+        <span>温馨提示：</span>
+        <span>设置定时提现后，仍可手动提现</span>
+      </div>
     </div>
     <!-- confirmTimePicker -->
     <TimePickerAction
       title="选择提现时间"
       v-model="state.planTime"
       v-model:visible="timePickerVisible"
+      :recentChoose="recentTime"
       :submit="confirmTimePicker" />
   </div>
   <overlay-loading :show="showOverlay" content="加载中..."></overlay-loading>
@@ -41,24 +50,29 @@
   import { queryTimingWithdrawalInfo, reviseTimingWithdrawal } from '@/api/wallet'
   type stateType = {
     planType: number // 1--每天
-    planTime: string
+    planTime?: string
+  }
+  interface editInfoType extends stateType {
+    shopAdminId: number
+    walletId: string
   }
   const timePickerVisible = ref(false)
   const showOverlay = ref(false)
   const oldState: stateType = reactive({
     planType: 0,
-    planTime: '00:00'
+    planTime: ''
   })
   const state: stateType = reactive({
     planType: 0,
-    planTime: '00:00'
+    planTime: ''
   })
   const handleSwitchChange = (val: number) => {
     console.log('switch')
     if (val) {
       timePickerVisible.value = true
     } else {
-      state.planTime = '00:00'
+      // MYTODO: 增加关闭提示弹框
+      // state.planTime = '00:00'
       state.planType = 0
       // 关闭定时提现选项
       submitTimePicker(val, '')
@@ -66,25 +80,25 @@
   }
   const submitTimePicker = (planType: number, planTime: string) => {
     showOverlay.value = true
-    const params = {
+    const params: editInfoType = {
       planType,
-      planTime: '',
       shopAdminId: 70,
-      walletId: 'QB00044964000001ff'
+      walletId: 'QB00044964000001'
     }
     params.planType === 1 && (params.planTime = planTime)
     return reviseTimingWithdrawal(params)
       .then(() => {
         console.log('chengg')
         state.planType = planType
-        state.planTime = planTime || '00:00'
+        planTime && (state.planTime = planTime)
         Object.assign(oldState, state)
         showOverlay.value = false
       })
       .catch(() => {
         console.log('shibai')
         state.planType = oldState.planType
-        state.planTime = oldState.planTime
+        // state.planTime = oldState.planTime
+        planType && (state.planTime = planTime)
         showOverlay.value = false
         return Promise.reject()
       })
@@ -98,7 +112,7 @@
       shopAdminId: 70,
       walletId: 'QB00044964000001'
     })
-    state.planTime = res.planTime || '00:00'
+    state.planTime = res.planTime || ''
     state.planType = res.planType
     Object.assign(oldState, state)
   })
@@ -116,12 +130,14 @@
     padding: 0 24px;
     background-color: #fff;
   }
+  .withdrawConfig-header-inner {
+    border-bottom: 1px solid $border-split-color-1;
+  }
   .withdrawConfig-header {
     display: flex;
-    justify-content: left;
+    justify-content: space-between;
     align-items: center;
     padding: 30px 0;
-    border-bottom: 1px solid $border-split-color-1;
   }
   .withdrawConfig-tips {
     display: inline-block;
@@ -133,15 +149,33 @@
     &-timing {
       display: inline-block;
       font-size: 28px;
-      padding: 20px;
-      margin-top: 26px;
       color: $primaryColor;
-      background-color: $painColor;
       font-weight: 500;
     }
     & > span {
       margin-top: 20px;
     }
+  }
+  .withdrawConfig-edit {
+    display: flex;
+    align-items: center;
+    margin: 10px 0 38px;
+  }
+  .withdrawConfig-icon-wrap {
+    color: $font-color-1;
+    padding: 0 30px 0 34px;
+    background-color: $bg-light-color-1;
+    height: 52px;
+    line-height: 52px;
+    border-radius: 26px;
+    margin-left: 20px;
+  }
+  .withdrawConfig-icon-info {
+    display: inline-block;
+    vertical-align: middle;
+    width: 30px;
+    height: 30px;
+    margin-right: 14px;
   }
   .withdrawConfig-footer {
     margin: 26px 24px 0;
@@ -150,7 +184,7 @@
     font-size: 32px;
     color: $font-color-1;
   }
-  .withdrawConfig-header-left {
+  .withdrawConfig-header-top {
     flex: 1;
   }
 </style>
