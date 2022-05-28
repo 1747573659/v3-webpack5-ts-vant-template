639e67de7f124acf7f9b58b46deadbf0fd144cb4
chensheng <chensheng@kemai.cn>
2022-05-28 15:29:49 +0800

    feat: 优化打包

--- vue.config.js
+++ vue.config.js
@@ -36,9 +36,36 @@ module.exports = defineConfig({
       return args;
     })
     config.plugins.delete('prefetch').delete('preload')
+
+    // 默认配置
+    // optimization: {
+    //   splitChunks: {
+    // 在cacheGroups外层的属性设定适用于所有缓存组，不过每个缓存组内部可以重设这些属性
+    //     chunks: "async", //将什么类型的代码块用于分割，三选一： "initial"：入口代码块 | "all"：全部 | "async"：按需加载的代码块
+    //     minSize: 30000, //大小超过30kb的模块才会被提取
+    //     maxSize: 0, //只是提示，可以被违反，会尽量将chunk分的比maxSize小，当设为0代表能分则分，分不了不会强制
+    //     minChunks: 1, //某个模块至少被多少代码块引用，才会被提取成新的chunk
+    //     maxAsyncRequests: 5, //分割后，按需加载的代码块最多允许的并行请求数，在webpack5里默认值变为6
+    //     maxInitialRequests: 3, //分割后，入口代码块最多允许的并行请求数，在webpack5里默认值变为4
+    //     automaticNameDelimiter: "~", //代码块命名分割符
+    //     name: true, //每个缓存组打包得到的代码块的名称
+    //     cacheGroups: {
+    //       vendors: {
+    //         test: /[\\/]node_modules[\\/]/, //匹配node_modules中的模块
+    //         priority: -10, //优先级，当模块同时命中多个缓存组的规则时，分配到优先级高的缓存组
+    //       },
+    //       default: {
+    //         minChunks: 2, //覆盖外层的全局属性
+    //         priority: -20,
+    //         reuseExistingChunk: true, //是否复用已经从原代码块中分割出来的模块
+    //       },
+    //     },
+    //   },
+    // },
+
     config.optimization.splitChunks({
       cacheGroups: {
-        libs: {
+        vendors: {
           name: 'chunk-vendor',
           chunks: 'initial',
           minChunks: 1,
@@ -47,27 +74,20 @@ module.exports = defineConfig({
           reuseExistingChunk: true,
           enforce: true
         },
-        common: {
+        commons: {
           name: 'chunk-common',
           chunks: 'initial',
-          minChunks: 2,
-          maxInitialRequests: 5,
-          minSize: 0,
+          minChunks: 2, // 被两个模块应用
+          maxInitialRequests: 3, // 分割后，入口代码块最多允许的并行请求数为5
+          minSize: 30000, // 超过30kb就被提取
           priority: 2,
           reuseExistingChunk: true,
           enforce: true
         },
-        vue: {
-          name: 'chunk-vue',
-          test: /[\\/]node_modules[\\/](vue|vue-router|vuex)[\\/]/,
-          chunks: 'all',
-          priority: 3,
-          reuseExistingChunk: true,
-          enforce: true
-        },
-        vant: {
-          name: 'chunk-vant',
-          test: /[\\/]node_modules[\\/]vant[\\/]/,
+        // 由于单个vue和vant都比较小，所以可以放在一起
+        vueandvant: {
+          name: 'chunk-vueandvant',
+          test: /[\\/]node_modules[\\/](vue|vue-router|vuex|vant)[\\/]/,
           chunks: 'all',
           priority: 3,
           reuseExistingChunk: true,
@@ -116,7 +136,8 @@ module.exports = defineConfig({
           deleteOriginalAssets: false // 是否删除源文件
         })
       )
-      config.plugins.push(new webpack.IgnorePlugin({resourceRegExp: /\.\/locale$/, contextRegExp: /moment$/}))
+      // 去掉moment的国际化的相关的内容
+      config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /\.\/locale$/, contextRegExp: /moment$/ }))
     }
   }
 })
